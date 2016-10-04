fd4e1140ad6151334039e2026c425ddb0a112688
TheThomasLeahy <thomasleahy.apps@gmail.com>
2016-10-04 18:54:42 -0500

    Scripts

--- Shell.m
+++ Shell.m
@@ -1,2 +1,26 @@
-%Project Shell
+%BME 370 Redesign Project
+%Team 9
+%MoleScope
+
+%Project Script
+
+
+%% Load Images
+
+%User input files
+%Load files
+
+%% Assess each image
+
+%For each image:
+%Border Detection
+%Calc Size
+%Calc Assymetry
+%Calc Color Variation
+
+
+%% Time-Lapse
+
+%Compare values from image to image
+%Create size contour map
 
