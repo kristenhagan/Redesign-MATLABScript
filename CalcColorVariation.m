d50257dee73bc0cf4b52bd7be969e992feb7036b
Laura Kenyon <laurakenyon@Lauras-MacBook-Pro.local>
2016-10-07 14:50:04 -0500

    u

--- CalcColorVariation.m
+++ CalcColorVariation.m
@@ -8,9 +8,32 @@ function [ output_args ] = CalcColorVariation( BorderXY, ImageBorder)
 % image(X)
 % colormap(map)
 
+colormap copper
+colorbar 
+subplot(2,3, 1)
+[A,map] = imread('image1.gif');
+imagesc(A)
 
-A = imread(colored_lesion1.gif)
-image(cdata)
+subplot(2,3, 2)
+B = imread('image2.gif');
+imagesc(B)
+
+subplot(2,3,3)
+C = imread('image3.gif');
+imagesc(C)
+
+subplot(2,3, 4)
+D = imread('image4.gif');
+imagesc(D)
+
+subplot(2,3, 5)
+E =imread('image5.gif');
+imagesc(E)
+
+subplot(2,3,6)
+F= imread('colored_lesion1.gif');
+imagesc(F)
+
 
 
 %imagesc(A)
