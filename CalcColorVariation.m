e70b3205d85a12ef217cd3ef1eddae41bd65af38
Laura Kenyon <laurakenyon@Lauras-MacBook-Pro.local>
2016-10-07 11:52:01 -0500

    update

--- CalcColorVariation.m
+++ CalcColorVariation.m
@@ -1,9 +1,27 @@
 function [ output_args ] = CalcColorVariation( BorderXY, ImageBorder)
 %Code by Laura Kenyon
-figure
-surf(peaks) %3D shaded surface plot
-colormap jet 
 
+
+% images2 = uigetdir('C:\', 'Select a folder of images you want to process!');
+% load images2
+% figure 
+% image(X)
+% colormap(map)
+
+imagesc(colormap)
+
+
+%surf(peaks) %3D shaded surface plot
+%colormap default
+%surf(colormap)
+
+% figure
+% a=[5 5 5; 4 4 4; 5 5 5];
+% cmap=colormap;
+% 
+% ax2 = subplot(2,1,2);
+% contour(a)
+% colormap(ax2,pink)
 
 end
 
